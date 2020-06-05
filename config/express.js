4ab0d5cc42657c1ab66f1b8d7817b1a6ec7bbbe2
shivachevv <31926786+shivachevv@users.noreply.github.com>
2020-06-05 16:13:32 +0300

    update

--- config/express.js
+++ config/express.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 module.exports = (app) => {
-    
-    //TODO: Setup the view engine
 
+    app.engine('.hbs', handlebars({
+        extname: ".hbs"
+    }))
+    app.set('view engine', '.hbs')
+    app.use('/static', express.static('static'))
     //TODO: Setup the body parser
 
-    //TODO: Setup the static files
 
 };
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// TODO: Require Controllers...
+const {
+    Router
+} = require('express')
+
+const router = Router()
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+module.exports = router
